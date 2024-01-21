2ee9de0242e698599601c0b8190cb47427abd95e
diego <dvizcarr@ucsc.edu>
2024-01-20 21:53:37 -0800

    new_post and working server.js

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
- const express = require('express');
- const app = express();
- const mongoose = require('mongoose');
- const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+app.use(express.urlencoded({extended: true}));
 
- app.use(bodyParser.urlencoded({extended: true}));
+mongoose.connect("mongodb+srv://dvizcarr:0uwHfVhhfFgmRHzd@cruzhacks.dpkyaf6.mongodb.net/CruzConnect", {useNewUrlParser: true}, {useUnifiedTopology: true})
 
-app.get('/', function(req, res){
-    res.send("express is working");
+const notesSchema = {
+    title: String,
+    description: String,
+    date: Date,
+    people: Number,
+    price: Number
+}
+const Note = mongoose.model('Note', notesSchema); 
+
+
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + '/new_post.html');
 })
 
-//app.post
- app.listen(3000, function(){
-    console.log('listening on port 3000');
- })
+app.post("/", function(req, res){
+    let newNote = new Note({
+        title: req.body.title,
+        description: req.body.description,
+        date: req.body.date,
+        people: req.body.people,
+        price: req.body.price
+    });
+    newNote.save();
+    res.redirect('/');
+})
+
+app.listen(3000, function () {
+    console.log('listening on port new');
+})
